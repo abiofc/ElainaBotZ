df3b80488c5c69f44f65f603ae0e3b35f769e08f
abiofc <108221611+abiofc@users.noreply.github.com>
2022-07-09 20:39:41 +0700

    Update setbotpp.js

--- plugins/setbotpp.js
+++ plugins/setbotpp.js
@@ -4,16 +4,36 @@
    Buatan: Miaweers
 */
 
-let handler = async (m, { conn, args }) => {
-    let bot = conn.user.jid // Bot
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `Foto tidak ditemukan`
-     conn.updateProfilePicture (bot, img)
-    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
-	}
+if (!isCreator) return m.reply(mess.owner)
+            if (!quoted) return m.reply(`Kirim/Reply Image Dengan Caption ${prefix + command}`)
+            if (!/image/.test(mime)) return m.reply(`Kirim/Reply Image Dengan Caption ${prefix + command}`)
+            if (/webp/.test(mime)) return m.reply(`Kirim/Reply Image Dengan Caption ${prefix + command}`)
+            var media = await hisoka.downloadAndSaveMediaMessage(quoted, 'ppbot.jpeg')
+            if (args[0] == `'panjang'`) {
+            var { img } = await generateProfilePicture(media)
+            await hisoka.query({
+            tag: 'iq',
+            attrs: {
+            to: botNumber,
+            type:'set',
+            xmlns: 'w:profile:picture'
+            },
+            content: [
+            {
+            tag: 'picture',
+            attrs: { type: 'image' },
+            content: img
+            }
+            ]
+            })
+            fs.unlinkSync(media)
+            m.reply(`Sukses`)
+            } else {
+            var data = await hisoka.updateProfilePicture(botNumber, { url: media })
+            fs.unlinkSync(media)
+            m.reply(`Sukses`)
+            }
+            }
     }
 handler.help = ['setbotpp']
 handler.command = /^(setbotpp)$/i
